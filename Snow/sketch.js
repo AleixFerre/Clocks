0eb8e9074f1e9a208c2f209cab6c08aa2ba0bc93
CatalaHD <thecatalahd@gmail.com>
2020-08-11 00:24:40 +0200

    Moving pad function

--- Snow/sketch.js
+++ Snow/sketch.js
@@ -97,11 +97,3 @@ function draw() {
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
-
-// Padding numbers function
-// See: https://stackoverflow.com/a/10073788/13295607
-function pad(n, width, z) {
-    z = z || '0';
-    n = n + '';
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-}
--- assets/Clock.js
+++ assets/Clock.js
@@ -1,3 +1,9 @@
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
 class Clock {
 
     constructor(smooth, smoothVel) {
